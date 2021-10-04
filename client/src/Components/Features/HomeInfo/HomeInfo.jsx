e12c038e5807fea5af09d2a65317b8b48a1b4544
David01051 <76750253+David01051@users.noreply.github.com>
2021-10-04 12:44:46 +0300

    fix

--- client/src/Components/Features/HomeInfo/HomeInfo.jsx
+++ client/src/Components/Features/HomeInfo/HomeInfo.jsx
@@ -7,11 +7,14 @@ export const HomeInfo = () => {
   return (
     <div  >
       <div className="title-info">
+      <div className="titles">
         <h1>{titles.titleForCorses}</h1>
+        <p></p>
+        </div>
       </div>
       <br />
       <div className="body-home-info">
-
+      <div className="continer-card">
         {informationForCorses.map((item, index) => {
           return (
             <div class="animate__animated animate__fadeInUp">
@@ -29,24 +32,30 @@ export const HomeInfo = () => {
           );
         })}
       </div>
+      </div>
       <br />
       <div className="title-info">
+        <div className="titles">
         <h1>{titles.titleCollaborations}</h1>
+        <p>תהליך המיון בטק-קריירה דומה לתהליך מיון ליחידות מובחרות, ואכן בסופו מתגבשת סיירת מופלאה, מפוצצת בכשרון ועם הרבה אש בעיניים. לכן זה כל כך טבעי שהחברות הכי מתקדמות יעבדו איתנו בשיתוף פעולה צמוד.<br></br><br></br> אלו הן חלק קטן מהחברות:</p>
+        </div>
       </div>
 
       <div className="body-home-company">
+        <div className="continer-company">
         {Collaborations.map((item, index) => {
           return (
             <div key={index} className="body-home-company-cards">
               <div >
                 <img src={item.img} />
                 <div className="info2">
-                  <p>{item.information}</p>
+                  {/* <p>{item.information}</p> */}
                 </div>
               </div>
             </div>
           );
         })}
+</div>
       </div>
     </div>
   );
